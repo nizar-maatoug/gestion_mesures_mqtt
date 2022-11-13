28b8ab883099730934434b702eda50a29c3ae186
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-13 16:44:29 +0100

    date view

--- gestion_mesures/gestion_mesures/urls.py
+++ gestion_mesures/gestion_mesures/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from website.views import welcome
+from website.views import welcome,date
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('welcome.html',welcome),
+    path('date',date)
 ]
--- gestion_mesures/website/views.py
+++ gestion_mesures/website/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('Welcome to mesures App')
+
+def date(request):
+    return HttpResponse('this page was served at '+ str(datetime.now()))
