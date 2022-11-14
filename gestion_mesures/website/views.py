3877624d19cd0c04ac74710db27fba64381311f6
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-14 16:11:01 +0100

    add template variable

--- gestion_mesures/website/views.py
+++ gestion_mesures/website/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def welcome(request):
-    return render(request,"website/welcome.html")
+    return render(request,"website/welcome.html",{"message":"this message was sent from view to template"})
 
 def date(request):
     return HttpResponse('this page was served at '+ str(datetime.now()))
