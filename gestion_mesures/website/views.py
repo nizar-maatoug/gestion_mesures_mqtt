294c5e20a58710c2df610e7334b8dc19b7eff245
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-14 16:26:23 +0100

    complete the MTV pattern

--- gestion_mesures/website/views.py
+++ gestion_mesures/website/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.shortcuts import render
 
+from website.models import Grandeur,Mesure
+
 # Create your views here.
 def welcome(request):
-    return render(request,"website/welcome.html",{"message":"this message was sent from view to template"})
+    return render(request,"website/welcome.html",{"num_grandeurs":Grandeur.objects.count()})
 
 def date(request):
     return HttpResponse('this page was served at '+ str(datetime.now()))
