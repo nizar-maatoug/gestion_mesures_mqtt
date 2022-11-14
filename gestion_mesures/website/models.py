84ce5e3b77c34dd9615d7a11f53f6fc0765d4e4b
nizar-maatoug <nizar.maatoug12@gmail.com>
2022-11-14 07:53:42 +0100

    add Grandeur model and run migrations

--- gestion_mesures/website/models.py
+++ gestion_mesures/website/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Grandeur(models.Model):
+    nom=models.CharField(max_length=60)
+    unite=models.CharField(max_length=30)
+    valeurMin=models.FloatField()
+    valeurMax=models.FloatField()
